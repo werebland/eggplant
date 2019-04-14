99d5621de1487f12a418a37c970c3b8fb810032b
itschrisabroad <christdmarshall@me.com>
2019-04-14 00:36:42 -0300

    procfile

---
+++ pages/profile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Profile extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default Profile;
